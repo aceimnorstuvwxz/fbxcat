6578a5e8fd95c3ca652dc838a75036ab619b4e46
Chen Bingfeng <iichenbf@gmail.com>
2015-05-15 01:25:52 +0800

    init xcode project

---
+++ libcat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libcat
+//
+//  Created by chenbingfeng on 15/5/15.
+//  Copyright (c) 2015年 chenbingfeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
